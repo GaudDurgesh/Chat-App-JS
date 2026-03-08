4780b71a473d9b6e1e7f330ae677a3c49641f50f
Durgesh <your-github-gaudd221@gmail.com>
2026-03-08 12:56:00 +0530

    online users implemented

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,8 +7,9 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
+
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -31,7 +32,7 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   connectDB();
 });
